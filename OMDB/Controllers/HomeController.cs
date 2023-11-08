c5ebfe17ee128a2e12fa1de8b4e9b7f7a6bbfabe
Irving Kirvan <kirvanirving@gmail.com>
2023-11-08 15:30:56 -0500

    Submission

---
+++ OMDB/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDB.Models;
+using System.Diagnostics;
+
+namespace OMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]    
+        public IActionResult MovieSearchForm()
+        {
+            return View("MovieSearch");
+        }
+
+        [HttpPost]
+        public IActionResult MovieSearchResults(string title)
+        {
+            Movie m = MovieDAL.GetMovie(title);
+            return View("MovieSearch", m);
+        }
+        [HttpGet]
+        public IActionResult MovieNightForm() 
+        {
+            return View("MovieNight");
+        }
+        [HttpPost]
+        public IActionResult MovieNightResults(string title1, string title2, string title3)
+        {
+            Movie m1 = MovieDAL.GetMovie(title1);
+            Movie m2 = MovieDAL.GetMovie(title2);
+            Movie m3 = MovieDAL.GetMovie(title3);
+            List<Movie> movies = new List<Movie>();
+            movies.Add(m1);
+            movies.Add(m2);
+            movies.Add(m3);
+            return View("MovieNight", movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMDB/Models/MovieDAL.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+
+namespace OMDB.Models
+{
+    public class MovieDAL
+    {   
+        public static Movie GetMovie(string title)
+        {
+            string url = $@"https://www.omdbapi.com/?t={title}&apikey=ea12151f";
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest();
+            Movie m = client.Get<Movie>(request);
+
+            return m;
+        }
+
+    }
+}
